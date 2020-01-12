1c924f0f2a768e24b3991eb9e66b64c606a89dd7
Serhii <kfzadrser@gmail.com>
2020-01-12 23:11:18 +0200

    add(link routes): add links functionality

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(express.json({ extended: true })) // middleware for parsing JSON body - 
 
 // routes
 app.use('/api/auth', require('./routes/auth.routes'))
-
+app.use('/api/link', require('./routes/link.routes'))
 
 const PORT = config.get('port') || 5000
 
